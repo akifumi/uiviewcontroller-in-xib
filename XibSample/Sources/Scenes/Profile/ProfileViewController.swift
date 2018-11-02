36b47e6a415e75fb84d2bb3b0f3e38785fe9ad9f
Akifumi Fukaya <akifumi.fukaya@gmail.com>
2018-11-02 19:29:57 +0900

    Created ProfileViewController.swift & ProfileViewController.xib

---
+++ XibSample/Sources/Scenes/Profile/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  XibSample
+//
+//  Created by akifumi.fukaya on 2018/11/02.
+//  Copyright © 2018年 Akifumi Fukaya. All rights reserved.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: Bundle.main)
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
