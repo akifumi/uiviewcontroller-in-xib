320d652743c52b4d4b042ba8540fb017f3e4c644
Akifumi Fukaya <akifumi.fukaya@gmail.com>
2018-11-02 19:14:51 +0900

    Setup xcode project.

---
+++ XibSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XibSample
+//
+//  Created by akifumi.fukaya on 2018/11/02.
+//  Copyright © 2018年 Akifumi Fukaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
